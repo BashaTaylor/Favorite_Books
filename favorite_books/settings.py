c5ff48f40bde4e1e0d8193c86a030335183df1cb
BashaTaylor <bashataylor.22@gmail.com>
2024-09-06 11:49:26 -0400

    u

--- favorite_books/settings.py
+++ favorite_books/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='fallback_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Favorite_Books.onrender.com', 'localhost']
 
